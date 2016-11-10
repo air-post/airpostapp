62b3cf3defc46d4ece83318bc8291aefa4da74f1
Charlie Chen <charlie.chen@fusionfactory.com>
2016-11-10 20:52:23 +1100

    fix error

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,2 +1,3 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :user_id, :latitude, :longitude, :altitude, :accuracy, :altitude_accuracy, :expire_time, :created_at
+end

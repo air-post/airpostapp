f9991074a28e884731e091665851a1a267975bae
Charlie Chen <charlie.chen@fusionfactory.com>
2016-11-10 20:50:38 +1100

    add more field for json return

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,2 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :title, :content
-end
+  attributes :id, :title, :content, :user_id, :latitude, :longitude, :altitude, :accuracy, :altitude_accuracy, :expire_time, :created_at

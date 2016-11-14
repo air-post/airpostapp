176d9512408d57b72719b5e7666a12a0ed429ddd
Charlie Chen <charlie.chen@fusionfactory.com>
2016-11-14 17:08:30 +1100

    Refactor model conditions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,28 +4,22 @@ class PostsController < ApplicationAPIController
 		Rails.logger.info "longitude query: #{params[:longitude].present?}"
 		Rails.logger.info "latitude query: #{params[:latitude].present?}"
 		Rails.logger.info "distance query: #{params[:distance].present?}"
-
-
-		if params[:longitude].present? and params[:latitude].present? and params[:distance].present?
-			posts = Post.where("select 3961000 * 2 * ASIN(SQRT(POWER(SIN((? - latitude) * pi()/180 / 2), 2) +COS(? * pi()/180) *COS(latitude * pi()/180) *POWER(SIN((? - longitude) * pi()/180 / 2), 2) )) <= ?", params[:latitude], params[:latitude], params[:longitude], params[:distance])
-		else
-			posts = Post.all
-		end
-		posts = posts.where('user_id = ?', params[:user_id]) if params[:user_id].present?
-
+		posts = Post.of_user(params[:user_id]).of_range(params[:latitude], params[:longitude], params[:distance])
+		# if params[:longitude].present? and params[:latitude].present? and params[:distance].present?
+		# 	posts = Post.where("select 3961000 * 2 * ASIN(SQRT(POWER(SIN((? - latitude) * pi()/180 / 2), 2) +COS(? * pi()/180) *COS(latitude * pi()/180) *POWER(SIN((? - longitude) * pi()/180 / 2), 2) )) <= ?", params[:latitude], params[:latitude], params[:longitude], params[:distance])
+		# end
+		#posts = posts.where('user_id = ?', params[:user_id]) if params[:user_id].present?
 		render json: posts
 	end
 
 	def create
 		post = Post.new(post_params)
-		post.user_id = post.user_id || 1
-		post.expire_time = post.expire_time || 10.days.from_now
 
 		post.save!
 		render json: post
 	end
 
 	def post_params
-    params.require(:post).permit(:title, :content, :user_id, :latitude, :longitude, :altitude, :accuracy, :altitude_accuracy, :expire_time)
-  end
+    	params.require(:post).permit(:title, :content, :user_id, :latitude, :longitude, :altitude, :accuracy, :altitude_accuracy, :expire_time)
+  	end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,15 @@
 class Post < ApplicationRecord
 	default_scope { where("not expired or expired is NULL ")}
 	default_scope { where("deleted_at is NULL ")} 
+	scope :of_range, -> (latitude, longitude, distance) { where("select 3961000 * 2 * ASIN(SQRT(POWER(SIN((? - latitude) * pi()/180 / 2), 2) +COS(? * pi()/180) *COS(latitude * pi()/180) *POWER(SIN((? - longitude) * pi()/180 / 2), 2) )) <= ?", latitude, latitude, longitude, distance) if latitude.present? and longitude.present? and distance.present?}
+	scope :of_user, -> (user_id) { where(user_id: user_id) if user_id.present? }
 
 	belongs_to :user
+
+	before_save :set_default
+
+	def set_default
+		self.user_id = self.user_id || 1
+		self.expire_time = self.expire_time || 10.days.from_now
+	end
 end
